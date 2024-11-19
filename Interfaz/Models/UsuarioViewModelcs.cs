890fb484c3b466ddcf7514ea5b3c66f4e35da60b
alexvalmu <valdelvira6@gmail.com>
2024-11-19 12:08:42 +0100

    uwu

---
+++ Interfaz/Models/UsuarioViewModelcs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Interfaz.Models
+{
+
+    public class LoginUsuarioViewModelcs
+    {
+        [Display(Prompt = "Introduce el dni del usuario",Description = "DNI usuario",Name = "DNIUsuario")]
+        [Required(ErrorMessage = "El DNI del Usuario es obligatorio")]
+
+        public string DNI { get; set; }
+        [Display(Prompt = "Introduce el password del usuario", Description = "Password usuario", Name = "Password")]
+        [DataType(DataType.Password)]
+
+        public string Password { get; set; }
+
+    }
+}
--- Interfaz/Startup.cs
+++ Interfaz/Startup.cs
@@ -50,7 +50,7 @@ namespace Interfaz
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Usuario}/{action=Login}/{id?}");
             });
         }
     }
