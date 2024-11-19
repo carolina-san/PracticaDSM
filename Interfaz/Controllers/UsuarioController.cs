b2401c035e5e62c9b934ff9e66e61ab234a03672
Carolina Sanchez <csa71@gcloud.ua.es>
2024-11-19 17:13:40 +0100

    Video login termianado

--- Interfaz/Controllers/UsuarioController.cs
+++ Interfaz/Controllers/UsuarioController.cs
@@ -1,13 +1,35 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DsmGen.ApplicationCore.CEN.Dominio_dsm;
+using DsmGen.Infraestructure.Repository.Dominio_dsm;
+using Interfaz.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Interfaz.Controllers
 {
-    public class UsuarioController1 : Controller
+    public class UsuarioController : Controller
     {
         // GET: UsuarioController/Login
         public ActionResult Login()
         {
+            return View();
+        }
+
+        // POST: UsuarioController/Login
+        [HttpPost]
+        public ActionResult Login(LoginUsuarioViewModel login)
+        {
+            UsuarioRepository usuRepo = new UsuarioRepository();
+            UsuarioCEN usuCEN = new UsuarioCEN(usuRepo);
+
+            if (usuCEN.Login(login.Email, login.Password) == null)
+            {
+                ModelState.AddModelError("", "Error al introducir los datos del email o Password");
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("index", "home");
+            }
             return View();
         }
 
---
+++ Interfaz/Models/UsuarioViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Interfaz.Models
+{
+    
+        public class LoginUsuarioViewModel
+        {
+            [Display(Prompt = "Introduce el email del usuario", Description = "Email usuario", Name = "EmailUsuario")]
+            [Required(ErrorMessage = "El email del Usuario es obligatorio")]
+
+            public string Email { get; set; }
+
+            [Display(Prompt = "Introduce el password del usuario", Description = "Password usuario", Name = "Password")]
+            [Required(ErrorMessage ="El Password del Usuario es obligatorio")]
+            [DataType(DataType.Password)]
+
+
+            public string Password { get; set; }
+
+        }
+    
+}
